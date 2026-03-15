4c2f5a61f9f2d85d9ae8cd9075598c10ce801fce
Dhruv Agja <dagja@homeserver.taild81631.ts.net>
2026-03-14 23:48:11 -0700

    [AI] fix: set 4GB heap for desktop-client build in fast-deploy-ui script

--- fast-deploy-ui.sh
+++ fast-deploy-ui.sh
@@ -15,7 +15,7 @@ echo "==> Building loot-core browser bundle..."
 yarn workspace loot-core build:browser
 
 echo "==> Building desktop-client browser bundle..."
-yarn workspace @actual-app/web build:browser
+NODE_OPTIONS=--max_old_space_size=4096 yarn workspace @actual-app/web build:browser
 
 echo "==> Copying build into running container..."
 docker cp packages/desktop-client/build/. actual-budget:/app/node_modules/@actual-app/web/build/
